822d31a5201bcc30933ee3c030eee1251e5cac7d
necati han <han.necati@gmail.com>
2023-10-20 13:05:20 +0200

    saved before organisation of file

---
+++ libft.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+
+//test
+int	test_isalpha(void);
+int	test_isdigit(void);
+int	test_isalnum(void);
+int	test_isascii(void);
+int	test_isprint(void);
+int	test_strlen(void);
+int	test_memset(void);
+int	test_bzero(void);
+int	test_memcpy(void);
+int	test_memmove(void);
+int	test_strlcpy(void);
+int	test_strlcat(void);
+
+
+//fonction
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n); 
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
