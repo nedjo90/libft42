88d1cb71dc19eb7b58dddf4ccbcbd45c8cea2f68
necati han <han.necati@gmail.com>
2023-10-28 16:24:22 +0200

    accomplished lstadd-back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhan <necat.han42@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 15:31:27 by nhan              #+#    #+#             */
+/*   Updated: 2023/10/28 16:23:18 by nhan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	last = *lst;
+	if (!last)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(last);
+		last->next = new;
+	}
+}
