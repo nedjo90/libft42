4ff77f999bc06ad9a0ff6070cf6ca7fda5b8732d
necati han <han.necati@gmail.com>
2023-10-28 12:41:57 +0200

    correction on split & substr

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nhan <necat.han42@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 19:35:00 by nhan              #+#    #+#             */
-/*   Updated: 2023/10/28 11:18:49 by nhan             ###   ########.fr       */
+/*   Updated: 2023/10/28 12:22:07 by nhan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,23 @@ char	**ft_free_tab(char **tab)
 	return (NULL);
 }
 
-char	*ft_strndup(const char *s1, size_t n)
+char	*ft_strndup(const char *s1, size_t len)
 {
 	char	*copy;
+	size_t	n;
+	size_t	i;
 
-	if (n > ft_strlen(s1))
-		n = ft_strlen(s1);
-	copy = (char *) malloc((n) * sizeof(char));
-	copy[--n] = '\0';
-	while ((int)n >= 0)
+	i = 0;
+	n = ft_strlen(s1);
+	if (n > len)
+		n = len;
+	copy = (char *)calloc(n + 1, sizeof(char));
+	if (!copy)
+		return (NULL);
+	while (i <= n)
 	{
-		copy[n] = s1[n];
-		n--;
+		copy[i] = s1[i];
+		i++;
 	}
 	return (copy);
 }
@@ -75,25 +80,21 @@ char	**ft_split(char const *s, char c)
 	int		k;
 
 	if (!s)
-		return (0);
+		return (NULL);
 	i = 0;
-	j = -1;
+	j = 0;
 	tab_str = (char **) malloc((ft_count_word(s, c) + 1) * sizeof(char *));
-	//char *temp = ft_itoa( ft_count_word(s, c));
-	//write(1, temp, ft_strlen(temp));
 	if (!tab_str)
 		return (0);
-	while (++j < ft_count_word(s, c))
+	while (j < ft_count_word(s, c))
 	{
 		while (s[i] == c && s[i])
 			i++;
 		k = i;
 		while (s[i] != '\0' && s[i] != c)
 			i++;
-		tab_str[j] = ft_substr(s, k, i - k);
-		if (j == ft_count_word(s, c))
-			break;
-		if (!tab_str[j])
+		tab_str[j++] = ft_substr(s, k, i - k);
+		if (!tab_str[j - 1])
 			return (ft_free_tab(tab_str));
 	}
 	tab_str[j] = NULL;
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nhan <necat.han42@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:22:30 by nhan              #+#    #+#             */
-/*   Updated: 2023/10/27 16:34:05 by nhan             ###   ########.fr       */
+/*   Updated: 2023/10/28 12:24:33 by nhan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = n - start;
 	if (len > n)
 		len = n;
-	str = (char *) malloc((char)len + 1);
+	str = (char *) malloc(len + 1);
 	if (!str)
 		return (NULL);
 	while (i < len && start < n)
