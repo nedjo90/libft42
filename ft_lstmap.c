6456130a274333f2ff66688fa1a2ab61222a53b2
hannecati <han.necati@gmail.com>
2023-10-29 13:30:48 +0100

    Finished with lstmap libft

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,41 +12,23 @@
 
 #include "libft.h"
 
-void	ft_lstclearlst(t_list *lst, void (*del)(void *))
-{
-	t_list	*temp;
-
-	if (!lst)
-		return ;
-	while(lst->next != NULL)
-	{
-		temp = lst->next;
-		del(lst->content);
-		free(lst);
-		lst = temp;
-	}
-	del(lst->content);
-	free(lst);
-	lst = NULL;
-}
-
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_temp;
 	t_list	*new_start;
+	void	*temp_content;
 
 	if (!lst || !(*f) || !(*del))
 		return (NULL);
 	new_start = NULL;
 	while (lst != NULL)
 	{
-		new_temp = ft_lstnew(f(lst->content));
+		temp_content = f(lst->content);
+		new_temp = ft_lstnew(temp_content);
 		if (new_temp == NULL)
 		{
-			free(new_temp);
-			ft_lstclearlst(new_start, (*del));
-			free(new_start);
+			del(temp_content);
+			ft_lstclear(&new_start, (*del));
 			return (NULL);
 		}
 		ft_lstadd_back(&new_start, new_temp);
